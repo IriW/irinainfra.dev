da78fda4f081cfee877afc39b4d39f4ae5f57d05
IriW <vzheshch@hotmail.com>
2025-05-27 20:59:16 +0200

    working state 27.05.2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Start',
+      description: 'From YAML to production: DevOps, Platforms, CI/CD & what I learn along the way.',
+      defaultLocale: 'en', // optional
+      locales: {
+        pl: { label: 'Polish', lang: 'pl' },
+        en: { label: 'English', lang: 'en' },
+        // de: { label: 'German', lang: 'de' },
+      },      
+      social: [
+        { icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/irinadebowska' },
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/IriW' },
+      ],
+    }),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@irinainfra_base': fileURLToPath(new URL('../irinainfra_base/src', import.meta.url)),
+      },
+    },
+  },
 });
